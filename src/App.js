8afb61742be217a1acb657778a7786649a8172d3
Enomahus <enomahus@outlook.com>
2026-01-28 09:06:45 +0100

    code source

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          Tempora illo a animi error magnam laborum iste eveniet repudiandae 
+          in quisquam iusto, unde, repellat minima quibusdam.
+        </h1>
+        
+      
+    </div>
+  );
+}
+
+export default App;
